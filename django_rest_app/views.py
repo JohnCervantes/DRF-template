def72c92c5d68cacfc10cc5e5c230fd7ddc8c37f
john Cervantes <johncamilocervantes@gmail.com>
2021-03-04 16:39:55 -0600

    added GET,POST,PATCH,DELETE,PUT methods, added authentication and permissions

--- django_rest_app/views.py
+++ django_rest_app/views.py
@@ -1,7 +1,10 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, viewsets
+from rest_framework.authentication import TokenAuthentication
 
+
+from .permissions import *
 from .serializers import *
 from .models import *
 
@@ -51,6 +54,8 @@ class HelloAPIView(APIView):
 class UserProfileAPIView(APIView):
     """ Handle craeting and updating user profiles """
     serializer_class = UserProfileSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (UpdateOwnProfile,)
 
     def get(self, request, id=None, format=None):
         """ Returns a list of User profiles"""
@@ -68,9 +73,35 @@ class UserProfileAPIView(APIView):
     def post(self, request):
         """ Create a User profile"""
         serializer = self.serializer_class(data=request.data)
-
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, id, pk=None):
+        """ Replace a user with a new User object model """
+        queryset = UserProfile.objects.get(id=id)
+        serializer = self.serializer_class(queryset, data=request.data)
+        self.check_object_permissions(request, queryset)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id, pk=None):
+        """ Delete a user model object """
+        queryset = UserProfile.objects.get(id=id)
+        queryset.delete()
+        self.check_object_permissions(request, queryset)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def patch(self, request, id, pk=None):
+        """ Replace a user with a new User object model """
+        queryset = UserProfile.objects.get(id=id)
+        serializer = self.serializer_class(
+            queryset, data=request.data, partial=True)
+        self.check_object_permissions(request, queryset)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
